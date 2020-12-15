d43f020965a79afc304fb6f78160f95285fce3b8
ValeriChakarov <valeritchakarov@gmail.com>
2020-12-15 19:00:05 +0000

    Spring Boot + JWT Token

---
+++ src/main/java/com/example/jwtokenapp/JwtokenappApplication.java
@@ -0,0 +1,13 @@
+package com.example.jwtokenapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtokenappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtokenappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jwtokenapp/JwtokenappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jwtokenapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtokenappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
